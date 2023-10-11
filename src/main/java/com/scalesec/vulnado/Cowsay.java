c78a05f92d731b60a90e785cd3f42b56552d6460
Julio Arruda <julio.arruda@outlook.com>
2023-10-11 16:39:31 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,13 +2,27 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class Cowsay {
+  private static final Logger LOGGER = Logger.getLogger(Cowsay.class.getName());
+
+  private Cowsay() {
+    // private constructor to hide the implicit public one
+  }
+
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
+    String sanitizedInput = sanitizeInput(input);
+    String cmd = "/usr/games/cowsay '" + sanitizedInput + "'";
+
+    if (System.getenv("DEBUG") == null) {
+      LOGGER.log(Level.INFO, cmd);
+    }
+
     processBuilder.command("bash", "-c", cmd);
+    processBuilder.environment().put("PATH", "/usr/games/");
 
     StringBuilder output = new StringBuilder();
 
@@ -21,8 +35,12 @@ public class Cowsay {
         output.append(line + "\n");
       }
     } catch (Exception e) {
-      e.printStackTrace();
+      LOGGER.log(Level.SEVERE, "Exception: ", e);
     }
     return output.toString();
+  }
+
+  private static String sanitizeInput(String input) {
+    return input.replaceAll("[^a-zA-Z0-9]", "");
   }
 }
