6bed02ba800dec2b7db780d774da27d4e708a382
Julio Arruda <julio.arruda@outlook.com>
2023-11-21 14:31:44 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/LinksController.java

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -1,22 +1,22 @@
 package com.scalesec.vulnado;
 
-import org.springframework.boot.*;
-import org.springframework.http.HttpStatus;
+// import org.springframework.boot.*; // Removido por GFT AI Impact Bot
+// import org.springframework.http.HttpStatus; // Removido por GFT AI Impact Bot
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
-import java.util.List;
-import java.io.Serializable;
+// import java.util.List; // Removido por GFT AI Impact Bot
+// import java.io.Serializable; // Removido por GFT AI Impact Bot
 import java.io.IOException;
 
 
 @RestController
 @EnableAutoConfiguration
 public class LinksController {
-  @RequestMapping(value = "/links", produces = "application/json")
+  @RequestMapping(value = "/links", method = RequestMethod.GET, produces = "application/json") // Alterado por GFT AI Impact Bot
   List<String> links(@RequestParam String url) throws IOException{
     return LinkLister.getLinks(url);
   }
-  @RequestMapping(value = "/links-v2", produces = "application/json")
+  @RequestMapping(value = "/links-v2", method = RequestMethod.GET, produces = "application/json") // Alterado por GFT AI Impact Bot
   List<String> linksV2(@RequestParam String url) throws BadRequest{
     return LinkLister.getLinksV2(url);
   }
