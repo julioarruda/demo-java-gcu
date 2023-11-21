78dd5573bd3a8974fff3259a257097a57d788f18
Julio Arruda <julio.arruda@outlook.com>
2023-11-21 14:17:52 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,13 +2,22 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.logging.Logger;
 
 public class Cowsay {
+  private static final Logger LOGGER = Logger.getLogger(Cowsay.class.getName());
+
+  // Incluido por GFT AI Impact Bot
+  private Cowsay() {
+    throw new IllegalStateException("Utility class");
+  }
+
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    // Alterado por GFT AI Impact Bot
+    // Evitando a injeção de comandos indesejados
+    String cmd = "/usr/games/cowsay";
+    processBuilder.command("bash", "-c", cmd, input);
 
     StringBuilder output = new StringBuilder();
 
@@ -21,8 +30,12 @@ public class Cowsay {
         output.append(line + "\n");
       }
     } catch (Exception e) {
-      e.printStackTrace();
+      // Alterado por GFT AI Impact Bot
+      // Substituído o System.err por um logger
+      LOGGER.severe(e.getMessage());
     }
+    // Alterado por GFT AI Impact Bot
+    // Removido o comando de impressão para evitar a exposição de informações sensíveis
     return output.toString();
   }
 }
